c5bd8344a8a2cd0d132a86d0041b3b205955efe9
gabe <gabenager@gmail.com>
2022-02-15 14:11:58 -0800

    test official pypi workflow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="autohyp",
-    version="0.0.1",
+    version="0.0.2",
     author="Gabriel Nager",
     author_email="gabenager@gmail.com.com",
     description="A simple hypothesis testing package",
